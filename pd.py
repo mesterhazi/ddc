dc1d3a1198b1a5743bc2044156351765197d0333
mesterhazi <adam.mesterhazi@gmail.com>
2019-08-11 22:32:51 +0200

    Added msot of the SCDC register + register read decoding + special cases

--- pd.py
+++ pd.py
@@ -35,6 +35,7 @@ class Decoder(srd.Decoder):
         self.offset = None  # offset is used in SCDC and HDCP register reads 
         self.protocol = None # 'scdc' or 'hdcp'
         self.databytes = [] # databytes
+        self.err_det_lower = None # for Character_Error_Detection registers this is the lower 7bits of the error value
         self.block_s = None  # start and end sample of a block
         self.block_e = None
 
@@ -46,15 +47,34 @@ class Decoder(srd.Decoder):
         reg_val = self.databytes[0] # one byte registers expected
         try:
             for i in range(len(SCDC_REG_LOOKUP[self.offset]['fields'])):
-                # something is wrong here
                 mask = SCDC_REG_LOOKUP[self.offset]['fields'][i]['mask']
-                field_interpretation = SCDC_REG_LOOKUP[self.offset]['fields'][i]['interpretation'][(reg_val & mask)]
-                messages.append(field_interpretation)
+                try:
+                    field_interpretation = SCDC_REG_LOOKUP[self.offset]['fields'][i]['interpretation'][(reg_val & mask)]
+                    messages.append(field_interpretation)
+                except TypeError:
+                    messages.append(SCDC_REG_LOOKUP[self.offset]['fields'][i]['interpretation'] + str(reg_val))
+                except KeyError:
+                    messages.append('Unexpected value in register with mask:{}'.format(mask))
                 
             self.put(self.ss, self.es, self.out_ann, [Annotations.fields, ['\n'.join(messages)]])
         except:
             pass
+        if self.offset in [0x50, 0x51, 0x52, 0x53, 0x54, 0x55]:  # Err det registers shall be read without setting new offset explicitly
+            if self.offset in [0x50, 0x52, 0x54]:  # First byte of the 2 byte registers
+                self.err_det_lower = reg_val
+                self.block_s = self.ss
 
+            elif self.offset in [0x51, 0x53, 0x55]:  # Second byte of the 2 byte registers
+                error_counter = self.err_det_lower + ((0x7F & reg_val) << 8)
+                channel = int ((self.offset - 0x51) / 2) # 0x51 is CH0, 0x53 is CH1, 0x55 is CH2
+                messages.append('Channel {} Error Counter = {}'.format(channel, error_counter))
+                messages.append('Ch{}_Valid = {}'.format(channel, reg_val >> 7))
+                self.put(self.block_s, self.es, self.out_ann, [Annotations.fields, ['\n'.join(messages)]])
+
+            elif self.offset == 0x56:
+                self.put(self.ss, self.es, self.out_ann, [Annotations.fields, ['Checksum of Character Error Detection registers']])
+            
+            self.offset += 1 # jump to the next Err det register
     def handle_EDID(self, data):
         pass
 
@@ -134,6 +154,7 @@ class Decoder(srd.Decoder):
             if cmd in ('DATA READ', 'DATA WRITE'):
                 self.read_or_write = cmd[5:]
                 self.databytes.append(databyte)
+                self.handle_message()
                 
 
             elif cmd in ['STOP', 'START REPEAT']:
@@ -145,11 +166,105 @@ class Decoder(srd.Decoder):
 
 # SCDC register lookup
 SCDC_REG_LOOKUP = {
-    0x20 : {
-        'name' : 'TMDS Config',
+    # Use the offset as key
+    0x01 : {
+        'name' : 'Sink version - R',  # Use 'name' as register name + the register type (R-ReadOnly, RW-Writable)
+        'fields' : [  # Use 'fields' as bitfields in the register
+            {'mask' : 0xFF, # Use 'mask' as the to select the bitfield
+             'interpretation' :     # Use 'interpretation' to interpret the different bitfield values to human readable format
+                                    {0x01 : 'Always 0x01 for HDMI2.0 compliant sinks'} 
+                                    # If the bitfield is not discrete value instead of the dict a Prefix string can be added
+            }   ]
+    },
+    0x02 : {
+        'name' : 'Source version - RW',
         'fields' : [
-            { 'mask' : 0x1, 'interpretation' : {0x1 : 'Scrambling Enable = ENABLED', 0x0 : 'Scrambling Enable = DISABLED'}},
-            { 'mask' : 0x2, 'interpretation' : {0x2 : 'TMDS_Bit_Clock_Ratio = 1/40', 0x0 : 'TMDS_Bit_Clock_Ratio = 1/10'}}
+            { 'mask' : 0xFF, 'interpretation' : {   0x01 : 'The Source is supporting HDMI2.0 SCDC registers', 
+                                                    0x00 : 'The Source is supporting HDMI2.0 SCDC registers'}}]
+    },
+    0x10 : {
+        'name' : 'Update_0 - R',
+        'fields' : [
+            {'mask' : 0x01, 'interpretation' : {0x01 : 'Status_Update=1 Indicating a change in the Status Registers',
+                                                0x00 : 'Status_Update=0 No change in the Staus Register'}},
+            {'mask' : 0x02, 'interpretation' : {0x02 : 'CED_Update=1 Indicating a change in the Character Error Detection Registers',
+                                                0x00 : 'CED_Update=0 No change in the Character Error Detection Register'}},
+            {'mask' : 0x04, 'interpretation' : {0x04 : 'RR_Test=1 Generate test Read Request',
+                                                0x00 : 'RR_Test=0 No no test Read Request'}},            
         ]
-    }    
+    },
+    0x11 : {
+        'name' : 'Update_1 - RW',
+        'fields' : [
+            {'mask' : 0xFF, 'interpretation' : 'Reserved to 0x00 in HDMI2.0b: '},
+        ]
+    },
+    0x20 : {
+        'name' : 'TMDS_Config - RW',
+        'fields' : [
+            { 'mask' : 0x01, 'interpretation' : {0x01 : 'Scrambling Enable = ENABLED', 0x00 : 'Scrambling Enable = DISABLED'}},
+            { 'mask' : 0x02, 'interpretation' : {0x02 : 'TMDS_Bit_Clock_Ratio = 1/40', 0x00 : 'TMDS_Bit_Clock_Ratio = 1/10'}}
+        ]
+    },
+    0x21 : {
+        'name' : 'Scrambler status - R',
+        'fields' : [
+            {'mask' : 0x01, 'interpretation' : {0x01 : 'Scrambling_Status = 1, Scrambled control code detected by the Sink',
+                                                0x00 : 'Scrambling_Status = 0, No scrambled control code detected by the Sink'}},
+        ]
+    },
+    0x30 : {
+        'name' : 'Config_0 - RW',
+        'fields' : [
+            {'mask' : 0x01, 'interpretation' : {0x01 : 'RR_Enable = 1, Source is supporting Read Request',
+                                                0x00 : 'RR_Enable = 0, Source only supports polling the Update Flags '}
+            }
+        ]
+    },
+    0x40 : {
+        'name' : 'Status_Flags_0 - R',
+        'fields' : [
+            {'mask' : 0x01, 'interpretation' : {0x01 : 'Clock_Detected = 1, Sink detected a valid clock signal.',
+                                                0x00 : 'Clock_Detected = 0, No valid clock signal detected by the Sink.'}},
+            {'mask' : 0x02, 'interpretation' : {0x02 : 'CH0_Locked = 1, Sink is successfully decoding data on HDMI Channel 0.',
+                                                0x00 : 'CH0_Locked = 0, Sink is not able to decode data on HDMI Channel 0.'}},
+            {'mask' : 0x04, 'interpretation' : {0x04 : 'CH1_Locked = 1, Sink is successfully decoding data on HDMI Channel 1.',
+                                                0x00 : 'CH1_Locked = 0, Sink is not able to decode data on HDMI Channel 1.'}},
+            {'mask' : 0x08, 'interpretation' : {0x08 : 'CH2_Locked = 1, Sink is successfully decoding data on HDMI Channel 2.',
+                                                0x00 : 'CH2_Locked = 0, Sink is not able to decode data on HDMI Channel 2.'}},
+        ]
+    },
+    0x41 : {
+        'name' : 'Status_Flags_1 - R',
+        'fields' : [
+            {'mask' : 0xFF, 'interpretation' : 'Reserved to 0x00 in HDMI2.0b: '},
+        ]
+    },
+    0x50 : {
+        'name' : 'Err_Det_0_L - R',
+    },
+    0x51 : {
+        'name' : 'Err_Det_0_H - R',
+    },
+    0x52 : {
+        'name' : 'Err_Det_1_L - R',
+    },
+    0x53 : {
+        'name' : 'Err_Det_1_H - R',
+    },
+    0x54 : {
+        'name' : 'Err_Det_2_L - R'
+    },
+    0x55 : {
+        'name' : 'Err_Det_2_H - R',
+    },
+    0x56 : {
+        'name' : 'Err_Det_Checksum - R',
+    },
+    # 0xC0 : {
+    #     'name' : 'Test_Config',
+    #     'fields' : [
+    #         {'mask' : , 'interpretation' : {}},
+    #     ]
+    # },
 }
