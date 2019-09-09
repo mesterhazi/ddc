02baadbdfb5635df16c847454f68f682285da565
adam.mesterhazi <adam.mesterhazi@lightware.com>
2019-09-09 13:11:18 +0200

    Added "verbosity" option - long explanations of field values are optional now

--- pd.py
+++ pd.py
@@ -16,6 +16,10 @@ class Decoder(srd.Decoder):
     license = 'gplv2+'
     inputs = ['i2c']
     outputs = ['scdc']
+    options = (
+        {'id': 'verbosity', 'desc': 'default: only register names | long: register names + explanations | debug: same as long + statemachine + i2c transactions ',
+        'default': 'short', 'values': ('short', 'long', 'debug')},
+    )
     annotations = ( ('Address', 'I²C address'),
                     ('Register', 'Register name and offset'),
                     ('Fields', 'Readable register interpretation'),
@@ -47,7 +51,10 @@ class Decoder(srd.Decoder):
                 mask = SCDC_REG_LOOKUP[self.offset]['fields'][i]['mask']
                 try:
                     field_interpretation = SCDC_REG_LOOKUP[self.offset]['fields'][i]['interpretation'][(reg_val & mask)]
-                    messages.append(field_interpretation)
+                    if self.options['verbosity'] in ['long', 'debug']:
+                        messages.append(''.join(field_interpretation))
+                    elif self.options['verbosity'] == 'short':
+                        messages.append(field_interpretation[0])
                 except TypeError:
                     messages.append(SCDC_REG_LOOKUP[self.offset]['fields'][i]['interpretation'] + str(reg_val))
                 except KeyError:
@@ -91,8 +98,8 @@ class Decoder(srd.Decoder):
         # store start and end samples
         self.ss = ss
         self.es = es
-
-        self.put(self.ss, self.es, self.out_ann, [Annotations.debug, [str(self.state) + ' ' + cmd]])
+        if self.options['verbosity'] == 'debug':
+            self.put(self.ss, self.es, self.out_ann, [Annotations.debug, [str(self.state) + ' ' + cmd]])
         # State machine.
         if self.state == States.IDLE:
             # Wait for an I²C START condition.
@@ -160,72 +167,72 @@ SCDC_REG_LOOKUP = {
         'fields' : [  # Use 'fields' as bitfields in the register
             {'mask' : 0xFF, # Use 'mask' as the to select the bitfield
              'interpretation' :     # Use 'interpretation' to interpret the different bitfield values to human readable format
-                                    {0x01 : 'Always 0x01 for HDMI2.0 compliant sinks'} 
+                                    {0x01 : ['Always 0x01 for HDMI2.0 compliant sinks', '']} 
                                     # If the bitfield is not discrete value instead of the dict a Prefix string can be added
             }   ]
     },
     0x02 : {
         'name' : 'Source version - RW',
         'fields' : [
-            { 'mask' : 0xFF, 'interpretation' : {   0x01 : 'The Source is supporting HDMI2.0 SCDC registers', 
-                                                    0x00 : 'The Source is supporting HDMI2.0 SCDC registers'}}]
+            { 'mask' : 0xFF, 'interpretation' : {   0x01 : ['Sink version = 1', ' - The Source is supporting HDMI2.0 SCDC registers'], 
+                                                    0x00 : ['Sink version = 0', ' - The Source is supporting HDMI2.0 SCDC registers']}}]
     },
     0x10 : {
         'name' : 'Update_0 - R',
         'fields' : [
-            {'mask' : 0x01, 'interpretation' : {0x01 : 'Status_Update=1 Indicating a change in the Status Registers',
-                                                0x00 : 'Status_Update=0 No change in the Staus Register'}},
-            {'mask' : 0x02, 'interpretation' : {0x02 : 'CED_Update=1 Indicating a change in the Character Error Detection Registers',
-                                                0x00 : 'CED_Update=0 No change in the Character Error Detection Register'}},
-            {'mask' : 0x04, 'interpretation' : {0x04 : 'RR_Test=1 Generate test Read Request',
-                                                0x00 : 'RR_Test=0 No no test Read Request'}},            
+            {'mask' : 0x01, 'interpretation' : {0x01 : ['Status_Update=1',  ' - Indicating a change in the Status Registers'],
+                                                0x00 : ['Status_Update=0',  ' - No change in the Staus Register']}},
+            {'mask' : 0x02, 'interpretation' : {0x02 : ['CED_Update=1', ' - Indicating a change in the Character Error Detection Registers'],
+                                                0x00 : ['CED_Update=0',  ' - No change in the Character Error Detection Register']}},
+            {'mask' : 0x04, 'interpretation' : {0x04 : ['RR_Test=1', ' - Generate test Read Request'],
+                                                0x00 : ['RR_Test=0', ' - No no test Read Request']}},            
         ]
     },
     0x11 : {
         'name' : 'Update_1 - RW',
         'fields' : [
-            {'mask' : 0xFF, 'interpretation' : 'Reserved to 0x00 in HDMI2.0b: '},
+            {'mask' : 0xFF, 'interpretation' : ['Reserved to 0x00 in HDMI2.0b: ', '']},
         ]
     },
     0x20 : {
         'name' : 'TMDS_Config - RW',
         'fields' : [
-            { 'mask' : 0x01, 'interpretation' : {0x01 : 'Scrambling Enable = ENABLED', 0x00 : 'Scrambling Enable = DISABLED'}},
-            { 'mask' : 0x02, 'interpretation' : {0x02 : 'TMDS_Bit_Clock_Ratio = 1/40', 0x00 : 'TMDS_Bit_Clock_Ratio = 1/10'}}
+            { 'mask' : 0x01, 'interpretation' : {0x01 : ['Scrambling Enable = ENABLED', ''], 0x00 : ['Scrambling Enable = DISABLED', '']}},
+            { 'mask' : 0x02, 'interpretation' : {0x02 : ['TMDS_Bit_Clock_Ratio = 1/40', ''], 0x00 : ['TMDS_Bit_Clock_Ratio = 1/10', '']}}
         ]
     },
     0x21 : {
         'name' : 'Scrambler status - R',
         'fields' : [
-            {'mask' : 0x01, 'interpretation' : {0x01 : 'Scrambling_Status = 1, Scrambled control code detected by the Sink',
-                                                0x00 : 'Scrambling_Status = 0, No scrambled control code detected by the Sink'}},
+            {'mask' : 0x01, 'interpretation' : {0x01 : ['Scrambling_Status = 1', ' - Scrambled control code detected by the Sink'],
+                                                0x00 : ['Scrambling_Status = 0', ' - No scrambled control code detected by the Sink']}},
         ]
     },
     0x30 : {
         'name' : 'Config_0 - RW',
         'fields' : [
-            {'mask' : 0x01, 'interpretation' : {0x01 : 'RR_Enable = 1, Source is supporting Read Request',
-                                                0x00 : 'RR_Enable = 0, Source only supports polling the Update Flags '}
+            {'mask' : 0x01, 'interpretation' : {0x01 : ['RR_Enable = 1', ' - Source is supporting Read Request'],
+                                                0x00 : ['RR_Enable = 0', ' - Source only supports polling the Update Flags ']}
             }
         ]
     },
     0x40 : {
         'name' : 'Status_Flags_0 - R',
         'fields' : [
-            {'mask' : 0x01, 'interpretation' : {0x01 : 'Clock_Detected = 1, Sink detected a valid clock signal.',
-                                                0x00 : 'Clock_Detected = 0, No valid clock signal detected by the Sink.'}},
-            {'mask' : 0x02, 'interpretation' : {0x02 : 'CH0_Locked = 1, Sink is successfully decoding data on HDMI Channel 0.',
-                                                0x00 : 'CH0_Locked = 0, Sink is not able to decode data on HDMI Channel 0.'}},
-            {'mask' : 0x04, 'interpretation' : {0x04 : 'CH1_Locked = 1, Sink is successfully decoding data on HDMI Channel 1.',
-                                                0x00 : 'CH1_Locked = 0, Sink is not able to decode data on HDMI Channel 1.'}},
-            {'mask' : 0x08, 'interpretation' : {0x08 : 'CH2_Locked = 1, Sink is successfully decoding data on HDMI Channel 2.',
-                                                0x00 : 'CH2_Locked = 0, Sink is not able to decode data on HDMI Channel 2.'}},
+            {'mask' : 0x01, 'interpretation' : {0x01 : ['Clock_Detected = 1', ' - Sink detected a valid clock signal.'],
+                                                0x00 : ['Clock_Detected = 0', ' - No valid clock signal detected by the Sink.']}},
+            {'mask' : 0x02, 'interpretation' : {0x02 : ['CH0_Locked = 1', ' - Sink is successfully decoding data on HDMI Channel 0.'],
+                                                0x00 : ['CH0_Locked = 0', ' - Sink is not able to decode data on HDMI Channel 0.']}},
+            {'mask' : 0x04, 'interpretation' : {0x04 : ['CH1_Locked = 1', ' - Sink is successfully decoding data on HDMI Channel 1.'],
+                                                0x00 : ['CH1_Locked = 0', ' - Sink is not able to decode data on HDMI Channel 1.']}},
+            {'mask' : 0x08, 'interpretation' : {0x08 : ['CH2_Locked = 1', ' - Sink is successfully decoding data on HDMI Channel 2.'],
+                                                0x00 : ['CH2_Locked = 0', ' - Sink is not able to decode data on HDMI Channel 2.']}},
         ]
     },
     0x41 : {
         'name' : 'Status_Flags_1 - R',
         'fields' : [
-            {'mask' : 0xFF, 'interpretation' : 'Reserved to 0x00 in HDMI2.0b: '},
+            {'mask' : 0xFF, 'interpretation' : ['Reserved to 0x00 in HDMI2.0b: ', '']},
         ]
     },
     0x50 : {
